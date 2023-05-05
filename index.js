c976946ac14ebd1f4b64c37b3856540c7644dd2f
Maximiliano Araya <maxi14@gmail.com>
2023-05-05 01:03:50 -0300

    Agregamos variables de entono

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const http = require('http');
 
 function requestController(){
@@ -8,4 +10,8 @@ console.log("Hola mundo!!!!");
 // Configurar nuestro servidor 
 const server = http.createServer(requestController);
 
-server.listen(4000);
+const PORT = process.env.PORT;
+
+server.listen(PORT, function (){
+    console.log("Aplicación corriendo en puerto: " + PORT)
+});
